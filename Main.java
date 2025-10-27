01ee280092244e46315b468b2b33fe82de624b06
СТРweb-pr <mager.romik@gmail.com>
2025-10-27 13:23:28 +0300

    произошло изменение кода

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static double calc_c_f(double c)
+    {
+        return c;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите значение температуры в градусах Цельсия: ");
+        double c=sc.nextDouble();
+        System.out.println(c+" градусов Цельсия = "+calc_c_f(c)+" по Фаренгейту");
+        }
+}
